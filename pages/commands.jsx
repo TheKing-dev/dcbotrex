9c4538ea87511ce37059ad8e4fb21b41d1ec72eb
TheKing-dev <133637624+TheKing-dev@users.noreply.github.com>
2024-08-04 16:55:51 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -65,7 +65,7 @@ const commands = [
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-  <Link href={"https://discord.com/oauth2/authorize?client_id=990709947509649468&permissions=8&integration_type=0&scope=bot"}>
+  <Link href={"https://discord.com/oauth2/authorize?client_id=972564795846983750&permissions=8&integration_type=0&scope=bot"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Botrex'i davet et</a>
   </Link>
